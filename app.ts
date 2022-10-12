5542447a759ed008e1a0419fccd87ceeb2386200
Jun Guo <gjuoun@gmail.com>
2022-10-12 11:34:26 -0300

    add es module lodash

--- app.ts
+++ app.ts
@@ -1,6 +1,9 @@
 import { Application } from "./mod.ts";
+import { add, eq } from 'https://deno.land/x/lodash@4.17.15-es/lodash.js';
 
 const app = new Application();
+
+console.log(eq(1,1 ))
 
 app.use((ctx) => {
   ctx.response.body = "Hello World! Jun";
--- mod.ts
+++ mod.ts
@@ -2,3 +2,5 @@ export { serve } from "https://deno.land/std@0.159.0/http/server.ts";
 // export * as lodash from "https://deno.land/x/lodash/mod.ts";
 
 export { Application } from "https://deno.land/x/oak/mod.ts";
+
+// export * as lodash from "https://deno.land/x/lodash@4.17.19/lodash.js";
